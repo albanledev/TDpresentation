98688cbc9e84b5ab0811895316f9637416ba09d8
Ateios <ateosnochamploo@gmail.com>
2022-09-17 20:20:13 +0200

    :art: rearranged some texts on ryan's page

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ function darkmode(){
 
 function darkmode_general(classNameElement){//ici le parametre permet de passer une donnée, une info à la fonction qui peut etre nommée de n'importe quelle façon
     let elements = document.querySelectorAll(classNameElement);//ne pas oublier; lorsque l'on utilise un query selector afin de récuperer quelque chose il faut mettre: # pour un id, . pour une class
-    for(let i=0; i<elements.length; i++){
+    for(let i=0; i<elements.length; i=i+1){
          if (elements[i].classList.contains("dark-mode")==false){//la fonction contains retourne la valeur false si la classe mise en parametre se trouve dans la liste de class de l'élément 
             elements[i].classList.add("dark-mode");
             localStorage.setItem("darkmode", "yes")//j'enregistre dans le localStorage le fait d'etre dans le mode dark

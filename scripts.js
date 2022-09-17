ebfcb357db649c9acb61e22d8388c0adc02e3e72
Ateios <ateosnochamploo@gmail.com>
2022-09-17 14:28:11 +0200

    :art: renamed the html and css files of my page

--- scripts.js
+++ scripts.js
@@ -2,15 +2,18 @@ function darkmode(){
     darkmode_general(".page-project");
     darkmode_general(".personality-block");
     darkmode_general(".likings-block");
-    darkmode_general(".project-block");   
+    darkmode_general(".project-block");
+    darkmode_general(".text-italic");
+       
 }
 function darkmode_general(classNameElement){//ici le parametre permet de passer une donnée, une info à la fonction qui peut etre nommée de n'importe quelle façon
-    let element = document.querySelector(classNameElement);//ne pas oublier; lorsque l'on utilise un query selector afin de récuperer quelque chose il faut mettre: # pour un id, . pour une class
-    if (element.classList.contains("dark-mode")==false){//la fonction contains retourne la valeur false si la classe mise en parametre se trouve dans la liste de class de l'élément 
-        element.classList.add("dark-mode");
+    let elements = document.querySelectorAll(classNameElement);//ne pas oublier; lorsque l'on utilise un query selector afin de récuperer quelque chose il faut mettre: # pour un id, . pour une class
+    for(let i=0; i<elements.length; i++)
+    if (elements[i].classList.contains("dark-mode")==false){//la fonction contains retourne la valeur false si la classe mise en parametre se trouve dans la liste de class de l'élément 
+        elements[i].classList.add("dark-mode");
     }
     else {
-        element.classList.remove("dark-mode");
+        elements[i].classList.remove("dark-mode");
     }
     
 }
